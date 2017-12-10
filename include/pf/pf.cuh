f838b387581f41bf8df9d8b9c297529fd5430421
Dany Haddad <danyhaddad43@gmail.com>
2017-12-10 17:47:15 -0600

    use curand and curand_kernel

--- include/pf/pf.cuh
+++ include/pf/pf.cuh
@@ -16,40 +16,40 @@ typedef struct systemModel {
     float_p_float_p_fptr step_process;
 } systemModel;
 
-float inner_product(float* vec1, float* vec2, int length);
+__device__ float inner_product(float* vec1, float* vec2, int length);
 
-float* vec_subtract(float* vec1, float* vec2, int length);
+__device__ float* vec_subtract(float* vec1, float* vec2, int length);
 
-float* mat_vec_mul(float* mat, float* vec, int input_length, int result_length);
+__device__ float* mat_vec_mul(float* mat, float* vec, int input_length, int result_length);
 
-float calc_norm_squared_in(float* vec, float* mat, int vec_length);
+__device__ float calc_norm_squared_in(float* vec, float* mat, int vec_length);
 
-void update_particle(systemModel model, float* current_state_estimate, float* current_measurement);
+__device__ void update_particle(systemModel model, float* current_state_estimate, float* current_measurement);
 
-float* pf(float* measurements, systemModel model, int num_samples, int num_particles);
+__device__ float* pf(float* measurements, systemModel model, int num_samples, int num_particles);
 
-void vec_mutate_divide(float* vec, float divisor, int length);
+__device__ void vec_mutate_divide(float* vec, float divisor, int length);
 
-float sum(float* vec, int length);
+__device__ float sum(float* vec, int length);
 
-float* cumsum(float* vec, int length);
+__device__ float* cumsum(float* vec, int length);
 
-void vec_mutate_add(float* vec1, float* vec2, int length);
+__device__ void vec_mutate_add(float* vec1, float* vec2, int length);
 
-float normal_rand();
+__device__ float normal_rand();
 
-float* random_normal_vector(int length);
+__device__ float* random_normal_vector(int length);
 
-void add_noise(float* vec, float* noise_covariance_sqrt, int length);
+__device__ void add_noise(float* vec, float* noise_covariance_sqrt, int length);
 
-float calc_unnormalized_importance_weight(systemModel model, float* current_state_estimate, float* current_measurement);
+__device__ float calc_unnormalized_importance_weight(systemModel model, float* current_state_estimate, float* current_measurement);
 
-void update_importance_weights(float* weights, systemModel model, float* current_measurement, float* particles, int num_particles);
+__device__ void update_importance_weights(float* weights, systemModel model, float* current_measurement, float* particles, int num_particles);
 
-void update_estimates(float* estimate, float* weights, float* particles, int num_particles, int num_state_variables);
+__device__ void update_estimates(float* estimate, float* weights, float* particles, int num_particles, int num_state_variables);
 
-float* resample_particles(float* particles, float* weights, int num_particles, int num_state_variables);
+__device__ float* resample_particles(float* particles, float* weights, int num_particles, int num_state_variables);
 
-void predict_particles_step(systemModel model, float* particles, int num_particles);
+__device__ void predict_particles_step(systemModel model, float* particles, int num_particles);
 
-float* initialize_particles(systemModel model, int num_particles);
+__device__ float* initialize_particles(systemModel model, int num_particles);
--- src/pf.cu
+++ src/pf.cu
@@ -2,8 +2,16 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cuda.h>
+#include <curand.h>
+#include <curand_kernel.h>
 
 #include "pf/pf.cuh"
+
+#define CUDA_CALL(x) do { if ((x) != cudaSuccess) {                     \
+            cout << " Error at " << __FILE__ << ":" << __LINE__ << endl; \
+            cout << " Error was " << x << " " << cudaGetErrorString(x) << endl; \
+            return EXIT_FAILURE ;}} while (0)                           \
 
 __device__ float inner_product(float* vec1, float* vec2, int length) {
     float acc = 0;
@@ -69,24 +77,18 @@ __device__ void vec_mutate_add(float* vec1, float* vec2, int length) {
     }
 }
 
-__device__ float normal_rand() {
-    float u = ((float) rand() / (RAND_MAX)) * 2 - 1;
-    float v = ((float) rand() / (RAND_MAX)) * 2 - 1;
-    float r = u * u + v * v;
-    if (r == 0 || r > 1) return normal_rand();
-    float c = sqrt(-2 * log(r) / r);
-    return u * c;
-}
-
-__device__ float* random_normal_vector(int length) {
+__device__ float* random_normal_vector(int length, curandState* state) {
     float* result = alloc_float(length);
     for (int vector_index = 0; vector_index < length; vector_index++) {
-        result[vector_index] = normal_rand();
+        result[vector_index] = curand_normal(state);
     }
     return result;
 }
 
-__device__ void add_noise(float* vec, float* noise_covariance_sqrt, int length) {
+__device__ void add_noise(float* vec,
+                          float* noise_covariance_sqrt,
+                          int length,
+                          curandState* state) {
     float* random = random_normal_vector(length);
     float* first_result = mat_vec_mul(noise_covariance_sqrt, random, length, length);
     vec_mutate_add(vec, first_result, length);
@@ -94,7 +96,9 @@ __device__ void add_noise(float* vec, float* noise_covariance_sqrt, int length) 
     free(random);
 }
 
-__device__ float calc_unnormalized_importance_weight(systemModel model, float* current_state_estimate, float* current_measurement) {
+__device__ float calc_unnormalized_importance_weight(systemModel model,
+                                                     float* current_state_estimate,
+                                                     float* current_measurement) {
     float* predicted_measurement = model.estimate_measurement(current_state_estimate);
     float* error = vec_subtract(current_measurement, predicted_measurement, model.num_measurement_variables);
     float unnormalized_weight = exp(-0.5 * calc_norm_squared_in(error,
@@ -105,7 +109,11 @@ __device__ float calc_unnormalized_importance_weight(systemModel model, float* c
     return unnormalized_weight;
 }
 
-__device__ void update_importance_weights(float* weights, systemModel model, float* current_measurement, float* particles, int num_particles) {
+__device__ void update_importance_weights(float* weights,
+                                          systemModel model,
+                                          float* current_measurement,
+                                          float* particles,
+                                          int num_particles) {
     for (int weight_index = 0; weight_index < num_particles; weight_index++) {
         float* current_state_estimate = &particles[weight_index * model.num_state_variables];
         weights[weight_index] = calc_unnormalized_importance_weight(model,
@@ -115,7 +123,11 @@ __device__ void update_importance_weights(float* weights, systemModel model, flo
     vec_mutate_divide(weights, sum(weights, num_particles), num_particles);
 }
 
-__device__ void update_estimates(float* estimate, float* weights, float* particles, int num_particles, int num_state_variables) {
+__device__ void update_estimates(float* estimate,
+                                 float* weights,
+                                 float* particles,
+                                 int num_particles,
+                                 int num_state_variables) {
     for (int variable_index = 0; variable_index < num_state_variables; variable_index++) {
         estimate[variable_index] = 0;
         for (int weight_index = 0; weight_index < num_particles; weight_index++) {
@@ -124,11 +136,15 @@ __device__ void update_estimates(float* estimate, float* weights, float* particl
     }
 }
 
-__device__ float* resample_particles(float* particles, float* weights, int num_particles, int num_state_variables) {
+__device__ float* resample_particles(float* particles,
+                                     float* weights,
+                                     int num_particles,
+                                     int num_state_variables,
+                                     curandState* state) {
     float* resampled_particles = alloc_float(num_particles * num_state_variables);
     float* reference = cumsum(weights, num_particles);
     for (int weight_index = 0; weight_index < num_particles; weight_index++) {
-        float uniform_sample = (weight_index + rand_f()) / num_particles;
+        float uniform_sample = (weight_index + curand_uniform(state)) / num_particles;
         int sum_index = 0;
         while (reference[sum_index] < uniform_sample) {
             sum_index++;
@@ -142,36 +158,45 @@ __device__ float* resample_particles(float* particles, float* weights, int num_p
     return resampled_particles;
 }
 
-__device__ void predict_particles_step(systemModel model, float* particles, int num_particles) {
-    for (int particle_index = 0; particle_index < num_particles * model.num_state_variables; particle_index += model.num_state_variables) {
-        float* particle = &particles[particle_index];
+__device__ void predict_particles_step(systemModel model,
+                                       float* particles,
+                                       int num_particles,
+                                       curandState* states) {
+    for (int particle_index = 0; particle_index < num_particles; particle_index++) {
+        float* particle = &particles[particle_index * model.num_state_variables];
         float* process_estimate = model.step_process(particle);
-        add_noise(process_estimate, model.process_noise_covariance_sqrt, model.num_state_variables);
+        add_noise(process_estimate, model.process_noise_covariance_sqrt, model.num_state_variables, &states[particle_index]);
         memcpy(particle, process_estimate, model.num_state_variables * sizeof(float));
         free(process_estimate);
     }
 }
 
-__device__ float* initialize_particles(systemModel model, int num_particles) {
+__device__ float* initialize_particles(systemModel model,
+                                       int num_particles,
+                                       curandState* states) {
     float* particles = alloc_float(num_particles * model.num_state_variables);
-    for (int particle_index = 0; particle_index < num_particles * model.num_state_variables; particle_index += model.num_state_variables) {
-        float* particle = &particles[particle_index];
+    for (int particle_index = 0; particle_index < num_particles; particle_index++) {
+        float* particle = &particles[particle_index * model.num_state_variables];
         memcpy(particle, model.initial_state, model.num_state_variables * sizeof(float));
-        add_noise(particle, model.initial_state_covariance_sqrt, model.num_state_variables);
+        add_noise(particle, model.initial_state_covariance_sqrt, model.num_state_variables, &states[particle_index]);
     }
     return particles;
 }
 
-__device__ float* pf(float* measurements, systemModel model, int num_samples, int num_particles) {
-    float* particles = initialize_particles(model, num_particles);
+__global__ float* pf(float* measurements,
+                     systemModel model,
+                     int num_samples,
+                     int num_particles,
+                     curandState* states) {
+    float* particles = initialize_particles(model, num_particles, state);
     float* weights = alloc_float(num_particles);
     float* estimates = alloc_float(num_samples * model.num_state_variables);
     for (int sample_index = 0; sample_index < num_samples; sample_index++) {
         float* current_measurement = &measurements[sample_index * model.num_measurement_variables];
         update_importance_weights(weights, model, current_measurement, particles, num_particles);
         update_estimates(&estimates[sample_index * model.num_state_variables], weights, particles, num_particles, model.num_state_variables);
-        particles = resample_particles(particles, weights, num_particles, model.num_state_variables);
-        predict_particles_step(model, particles, num_particles);
+        particles = resample_particles(particles, weights, num_particles, model.num_state_variables, &states[particle_index]);
+        predict_particles_step(model, particles, num_particles, &states[particle_index]);
     }
     free(weights);
     free(particles);
