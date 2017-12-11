66c11be781f40eab89f12264b09cea73e89eaced
Dany Haddad <danyhaddad43@gmail.com>
2017-12-10 18:10:16 -0600

    tat

--- include/pf/pf.cuh
+++ include/pf/pf.cuh
@@ -1,3 +1,3 @@
-#include "./pf_support.h"
+#include "pf_support.cuh"
 
 __global__ void pf(float* estimates, float* measurements, systemModel model, int num_samples, int num_particles, curandState* states);
