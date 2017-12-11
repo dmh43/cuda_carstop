87f104f096f6ad0d1d80afdc5c6e68414694faee
Dany Haddad <danyhaddad43@gmail.com>
2017-12-10 18:03:24 -0600

    remove device declarations

--- include/pf/pf.cuh
+++ include/pf/pf.cuh
@@ -16,40 +16,40 @@ typedef struct systemModel {
     float_p_float_p_fptr step_process;
 } systemModel;
 
-__device__ float inner_product(float* vec1, float* vec2, int length);
+float inner_product(float* vec1, float* vec2, int length);
 
-__device__ float* vec_subtract(float* vec1, float* vec2, int length);
+float* vec_subtract(float* vec1, float* vec2, int length);
 
-__device__ float* mat_vec_mul(float* mat, float* vec, int input_length, int result_length);
+float* mat_vec_mul(float* mat, float* vec, int input_length, int result_length);
 
-__device__ float calc_norm_squared_in(float* vec, float* mat, int vec_length);
+float calc_norm_squared_in(float* vec, float* mat, int vec_length);
 
-__device__ void update_particle(systemModel model, float* current_state_estimate, float* current_measurement);
+void update_particle(systemModel model, float* current_state_estimate, float* current_measurement);
 
-__device__ void vec_mutate_divide(float* vec, float divisor, int length);
+void vec_mutate_divide(float* vec, float divisor, int length);
 
-__device__ float sum(float* vec, int length);
+float sum(float* vec, int length);
 
-__device__ float* cumsum(float* vec, int length);
+float* cumsum(float* vec, int length);
 
-__device__ void vec_mutate_add(float* vec1, float* vec2, int length);
+void vec_mutate_add(float* vec1, float* vec2, int length);
 
-__device__ float normal_rand();
+float normal_rand();
 
-__device__ float* random_normal_vector(int length, curandState* state);
+float* random_normal_vector(int length, curandState* state);
 
-__device__ void add_noise(float* vec, float* noise_covariance_sqrt, int length, curandState* state);
+void add_noise(float* vec, float* noise_covariance_sqrt, int length, curandState* state);
 
-__device__ float calc_unnormalized_importance_weight(systemModel model, float* current_state_estimate, float* current_measurement);
+float calc_unnormalized_importance_weight(systemModel model, float* current_state_estimate, float* current_measurement);
 
-__device__ void update_importance_weights(float* weights, systemModel model, float* current_measurement, float* particles, int num_particles);
+void update_importance_weights(float* weights, systemModel model, float* current_measurement, float* particles, int num_particles);
 
-__device__ void update_estimates(float* estimate, float* weights, float* particles, int num_particles, int num_state_variables);
+void update_estimates(float* estimate, float* weights, float* particles, int num_particles, int num_state_variables);
 
-__device__ float* resample_particles(float* particles, float* weights, int num_particles, int num_state_variables, curandState* state);
+float* resample_particles(float* particles, float* weights, int num_particles, int num_state_variables, curandState* state);
 
-__device__ void predict_particles_step(systemModel model, float* particles, int num_particles, curandState* states);
+void predict_particles_step(systemModel model, float* particles, int num_particles, curandState* states);
 
-__device__ float* initialize_particles(systemModel model, int num_particles, curandState* state);
+float* initialize_particles(systemModel model, int num_particles, curandState* state);
 
-__global__ void pf(float* estimates, float* measurements, systemModel model, int num_samples, int num_particles, curandState* states);
+void pf(float* estimates, float* measurements, systemModel model, int num_samples, int num_particles, curandState* states);
