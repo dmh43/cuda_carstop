fe5c1dcc796f76396b889eff7af12167f0c6e37e
Dany Haddad <danyhaddad43@gmail.com>
2017-12-11 10:49:48 -0600

    semicolon

--- test/test_helper.cuh
+++ test/test_helper.cuh
@@ -1,3 +1,3 @@
 void inner_product_kernel(float* vec1, float* vec2, int length, float* result);
 
-float gpu_inner_product(float* vec1, float* vec2, int length)
+float gpu_inner_product(float* vec1, float* vec2, int length);
