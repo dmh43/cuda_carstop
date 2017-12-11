b93669d367ff88ca3a7a5650604ec071f8ecd10b
Dany Haddad <danyhaddad43@gmail.com>
2017-12-11 10:39:12 -0600

    try that

--- test/pf_test.cc
+++ test/pf_test.cc
@@ -29,7 +29,7 @@ namespace {
     }
 
     void inner_product_kernel(float* vec1, float* vec2, int length, float* result) {
-        result* = inner_product(vec1, vec2, length);
+        *result = inner_product(vec1, vec2, length);
     }
 
     float gpu_inner_product(float* vec1, float* vec2, int length) {
@@ -37,10 +37,11 @@ namespace {
         int size = sizeof(float) * length;
         cudaMalloc(&gpu_vec1, size);
         cudaMalloc(&gpu_vec2, size);
+        cudaMalloc(&result, sizeof(float));
         cudaMemcpy(gpu_vec1, vec1, size, cudaMemcpyHostToDevice);
         cudaMemcpy(gpu_vec2, vec2, size, cudaMemcpyHostToDevice);
         inner_product_kernel<<<1, 1>>>(gpu_vec1, gpu_vec2, length, result);
-        return result*;
+        return *result;
     }
 
     TEST(InnerProductTest, NormSquared) {
