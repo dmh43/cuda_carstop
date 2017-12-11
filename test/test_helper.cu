4806e823db676f356b059d792297c0d9ae9ebe92
Dany Haddad <danyhaddad43@gmail.com>
2017-12-11 10:46:42 -0600

    create src file

---
+++ test/test_helper.cu
@@ -0,0 +1,18 @@
+#include <cuda.h>
+#include "pf/pf_support.cuh"
+
+void inner_product_kernel(float* vec1, float* vec2, int length, float* result) {
+    *result = inner_product(vec1, vec2, length);
+}
+
+float gpu_inner_product(float* vec1, float* vec2, int length) {
+    float *gpu_vec1, *gpu_vec2, *result;
+    int size = sizeof(float) * length;
+    cudaMalloc(&gpu_vec1, size);
+    cudaMalloc(&gpu_vec2, size);
+    cudaMalloc(&result, sizeof(float));
+    cudaMemcpy(gpu_vec1, vec1, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(gpu_vec2, vec2, size, cudaMemcpyHostToDevice);
+    inner_product_kernel<<<1, 1>>>(gpu_vec1, gpu_vec2, length, result);
+    return *result;
+}
