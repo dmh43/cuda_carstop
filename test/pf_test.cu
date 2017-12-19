025c4443a0de2a163222cbe23095cae170a54fa4
Dany Haddad <danyhaddad43@gmail.com>
2017-12-19 10:15:04 -0600

    void star

--- test/pf_test.cu
+++ test/pf_test.cu
@@ -30,7 +30,6 @@ namespace {
 
     __global__ void vec_subtract_kernel(float* vec1, float* vec2, int length, float** result) {
         *result = vec_subtract(vec1, vec2, length);
-        printf("%f\n", 2.0f);
     }
 
     float* run_kernel_vec_subtract(float* vec1, float* vec2, int length) {
@@ -43,9 +42,10 @@ namespace {
         cudaMemcpy(gpu_vec1, vec1, size, cudaMemcpyHostToDevice);
         cudaMemcpy(gpu_vec2, vec2, size, cudaMemcpyHostToDevice);
         vec_subtract_kernel<<<1, 1>>>(gpu_vec1, gpu_vec2, length, result_dev);
-        // cudaMemcpy(result_host, *result_dev, size, cudaMemcpyDeviceToHost);
-        // printf("%f\n", result_host[0]);
-        // printf("%f\n", result_host[1]);
+        printf("%p\n", (void*) *result_dev);
+        cudaMemcpy(result_host, *result_dev, size, cudaMemcpyDeviceToHost);
+        printf("%f\n", result_host[0]);
+        printf("%f\n", result_host[1]);
         return result_host;
     }
 
